63bd6e8fd886544ecb2938d7a7015238b4ffdf72
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2018-12-22 12:46:11 -0200

    Consulta e Exclusao de tarefas

--- app/src/todo/todo.jsx
+++ app/src/todo/todo.jsx
@@ -17,6 +17,17 @@ export default class Todo extends Component {
 
     this.handleAdd = this.handleAdd.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+
+    this.refresh();
+  }
+
+  refresh() {
+    axios
+      .get(`${URL}?sort=-createdAt`)
+      .then(resp =>
+        this.setState({ ...this.state, description: '', list: resp.data })
+      );
   }
 
   handleChange(e) {
@@ -26,7 +37,11 @@ export default class Todo extends Component {
   handleAdd() {
     const { description } = this.state;
 
-    axios.post(URL, { description }).then(res => console.log('Funcionou'));
+    axios.post(URL, { description }).then(res => this.refresh());
+  }
+
+  handleRemove(todo) {
+    axios.delete(`${URL}/${todo._id}`).then(res => this.refresh());
   }
 
   render() {
@@ -38,7 +53,7 @@ export default class Todo extends Component {
           description={this.state.description}
           handleChange={this.handleChange}
         />
-        <TodoList />
+        <TodoList list={this.state.list} handleRemove={this.handleRemove}/>
       </div>
     );
   }
