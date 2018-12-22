2082ab4268415c802cb1a99124736e71fdeb13be
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2018-12-22 11:20:44 -0200

    Renderizacao condicional

--- app/src/todo/todo.jsx
+++ app/src/todo/todo.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import PageHeader from '../template/PageHeader';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
 
 export default class todo extends Component {
   render() {
     return (
       <div>
-        <PageHeader name='Tarefas' small='Cadastro' />
+        <PageHeader name="Tarefas" small="Cadastro" />
+        <TodoForm />
+        <TodoList />
       </div>
-    )
+    );
   }
 }
-
