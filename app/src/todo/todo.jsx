6348b83bcfcf755a20cc0fec4004981b45f1a2d2
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2018-12-22 11:51:02 -0200

    Adiconar integrando BackEnd

--- app/src/todo/todo.jsx
+++ app/src/todo/todo.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import PageHeader from '../template/PageHeader';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
+
+const URL = 'http://localhost:3003/api/todos';
 
 export default class Todo extends Component {
   constructor() {
@@ -20,7 +24,9 @@ export default class Todo extends Component {
   }
 
   handleAdd() {
-    console.log(this.state.description)
+    const { description } = this.state;
+
+    axios.post(URL, { description }).then(res => console.log('Funcionou'));
   }
 
   render() {
