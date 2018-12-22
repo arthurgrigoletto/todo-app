45a6ad3aae87ed3791ddf65aa5f221648413a49e
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2018-12-22 11:32:03 -0200

    Evento Adicionar

--- app/src/todo/todo.jsx
+++ app/src/todo/todo.jsx
@@ -4,11 +4,21 @@ import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 export default class todo extends Component {
+  constructor() {
+    super();
+
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd() {
+    console.log(this);
+  }
+
   render() {
     return (
       <div>
         <PageHeader name="Tarefas" small="Cadastro" />
-        <TodoForm />
+        <TodoForm handleAdd={this.handleAdd} />
         <TodoList />
       </div>
     );
